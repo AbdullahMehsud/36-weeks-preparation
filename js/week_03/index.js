6a6a408659f7d09a5eeb812afd8dbabddd80d7a8
AbdullahMehsud <abdullahmsd85@gmail.com>
2025-04-24 23:45:39 +0500

    patch api

--- js/week_03/index.js
+++ js/week_03/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json')
+const fs = require('fs')
 const app = express();
-const PORT = 8000;
+const PORT = 5000;
 // route for html
 // app.get('/users', (req, res) => {
 //     const html = 
@@ -11,14 +12,20 @@ const PORT = 8000;
 //     res.send(html)
 // })
 
-
+app.use(express.urlencoded({extended: false}))
 // Basic route
-app.get('api/users', (req, res) => {
+app.get('/api/users', (req, res) => {
   return res.json(users)
 });
 
 // get user by id 
-
+app.post('/api/users', (req, res) => {
+    const user = req.body
+    users.push({...user, id: users.length + 1})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+      return res.json({status: "success"})
+    })
+})
 app.route('/api/users/:id')
 .get( (req, res) => {
     const id = Number(req.params.id)
@@ -27,11 +34,29 @@ app.route('/api/users/:id')
 })
 .patch((req, res) => {
     //TODO: edit user
-    return res.json({status: "pending"})
+    const id = Number(req.params.id)
+    const userIndex = users.findIndex(user => user.id === id)
+    const updateUser = {...users[userIndex], ...req.body}
+    console.log(updateUser);
+    
+    users[userIndex] = updateUser
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        
+      return res.json({status: "success", user: updateUser})
+    })
+    
+    
 })
 .delete((req, res) => {
     //TODO: delete user
-    return res.json({status: "pending"})
+    const id = Number(req.params.id)
+    const usserIndex = users.findIndex(user => user.id === id)
+    const update = users.splice(usserIndex, 1)
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({status: "success"})
+    })
+    // return res.json({status: "success", user: update })
 })
 
 // Start server
