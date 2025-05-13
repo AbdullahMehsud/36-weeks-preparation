8b0f9580c22d9ebb08556efb40c0a31b345fd651
AbdullahMehsud <abdullahmsd85@gmail.com>
2025-05-13 23:58:54 +0500

    minor changes

---
+++ js/week_04/react_counter_app/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='main-div'>
+      <h1 className='first-h1-tag'> hello world</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
